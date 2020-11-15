131ea88c719ad17ff5b5a0437435df3275f36237
KhalezovaMarianna <halezova_mariann@mail.ru>
2020-11-16 02:07:41 +0300

    5 commit

--- src/bsu/rfe/java/group1/lab1/Khalezova/var9A/Main.java
+++ src/bsu/rfe/java/group1/lab1/Khalezova/var9A/Main.java
@@ -79,7 +79,7 @@ public class Main extends JFrame {
         textFieldZ.setMaximumSize(textFieldZ.getPreferredSize());
         Box hboxVariables = Box.createHorizontalBox();
         hboxVariables.setBorder(
-                BorderFactory.createLineBorder(Color.RED));//создание кнопок
+                BorderFactory.createLineBorder(Color.RED));
         hboxVariables.add(labelForX);
         hboxVariables.add(Box.createHorizontalStrut(10));
         hboxVariables.add(textFieldX);
